748e2be6d7587c9dc22a24179c108f1bbe1f852b
krany4 <krasniy.vladim@yandex.ru>
2023-02-15 08:52:07 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,76 @@
+// function logger() {
+// 	console.log(" My name is Vova");
+// }
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+// 	const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+// 	return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangesJuice = fruitProcessor(2, 4);
+// console.log(appleOrangesJuice);
+
+// function calcAge1(birthYear) {
+// 	return 2022 - birthYear;
+// }
+
+// const age1 = calcAge1(20);
+// console.log(age1);
+
+// const calc3 = (birthYear) => 2022 - birthYear;
+// const age4 = calc3;
+// console.log(age4);
+
+// const yearsUntil = (birthYear, firstName) => {
+// 	const age = 2023 - birthYear;
+// 	const ret = 65 - age;
+// 	// return ret;
+// 	return `${firstName} go to pet cherez ${ret} years`;
+// };
+// const result = yearsUntil(2002, "Vova");
+// console.log(result);
+// console.log(yearsUntil(2002));
+
+// function fruitProcessor(apples, oranges) {
+// 	const applePiece = cutFruit(apples);
+// 	const orangePiece = cutFruit(oranges);
+// 	const juice = `Juice with ${applePiece} apples and ${orangePiece} oranges`;
+// 	return juice;
+// }
+
+// const cutFruit = (fruit) => {
+// 	return fruit * 4;
+// };
+
+// const result2 = fruitProcessor(2, 3);
+// console.log(result2);
+
+// const hello = (name) => {
+// 	return `Hello, my name is ${name}`;
+// };
+// const privet = hello("Vova");
+// console.log(privet);
+
+// const calcAverage = (a, b, c) => {
+// 	return (a + b + c) / 3;
+// };
+// const result = calcAverage(5, 5, 5);
+// console.log(result);
+// const result1 = calcAverage(6, 6, 6);
+// console.log(result1);
+
+// const checkAverage = (result, result1) => {
+// 	if (result > result1) {
+// 		console.log("TEam 1 win");
+// 	} else {
+// 		console.log("Team2 win");
+// 	}
+// };
+// checkAverage(result, result1);
+// checkAverage(100, 2)
